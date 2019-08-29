a9ac21f482a449a03264601422e24ff309100a28
MatiChillo <chillo.matias@gmail.com>
2019-08-29 13:31:06 -0300

    Corrección de compra en Controller

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -107,7 +107,7 @@ class ProductsController extends Controller
     ->get();
     return view("search", compact("products"));
   }
-}
+
 
 
   // $userSaved = user::create($request->all());
@@ -123,8 +123,8 @@ class ProductsController extends Controller
   // 		return redirect('/perfil_usuario');
   // 	}
 
-//   public function purchase(){
-//     return redirect("/products");
-//   }
-//
-// }
+  public function purchase(){
+    return redirect("/products");
+  }
+
+}
